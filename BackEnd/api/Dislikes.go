7e0c1fcaf9533721e5c034f5d33a78821606f506
zsalhi <rafayelle2005@gmail.com>
2024-11-22 15:58:02 +0100

    add likes and dislikes api

--- BackEnd/api/Dislikes.go
+++
@@ -1,6 +0,0 @@
-package api
-
-import "net/http"
-
-func DislikesAPI(w http.ResponseWriter, r *http.Request) {
-}
--- BackEnd/api/Likes.go
+++ BackEnd/api/Likes.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
 	"strconv"
 
@@ -10,20 +9,51 @@ import (
 )
 
 func AddLikeAPI(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
+	if r.Method != http.MethodPost {
 		utils.Writer(w, map[string]string{"Error": "Methode not allowed"}, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	counter := 0
-	Err := db.Db.QueryRow("select is_like from likes_dislikes where is_like = true")
-
-	Err.Scan(counter)
-
-	Res, err := json.Marshal(map[string]string{"Counter:": strconv.Itoa(counter)})
+	UserID, err := GetUserID(r)
 	if err != nil {
-		utils.Writer(w, map[string]string{"Error": err.Error()}, http.StatusBadRequest)
+		utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
 		return
 	}
-	w.Write(Res)
+	PostID := r.FormValue("PostID")
+	IsComment := r.FormValue("IsComment")
+	IsLike := r.FormValue("IsLike")
+
+	_, err = db.Db.Exec("INSERT INTO likes_dislikes (post_id, user_id, is_like, is_comment) VALUES (?, ?, ?, ?)", PostID, UserID, IsLike, IsComment)
+	if err != nil {
+		utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
+		return
+	}
+	var PostLikesCounter, PostDislikesCounter, CommentsLikeCounter, CommentsDislikesCounter int
+
+	err = db.Db.QueryRow("SELECT COUNT(*) FROM likes_dislikes WHERE is_like = TRUE AND is_comment = FALSE").Scan(&PostLikesCounter)
+	if err != nil {
+		utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
+		return
+	}
+	err = db.Db.QueryRow("SELECT COUNT(*) FROM likes_dislikes WHERE is_like = FALSE AND is_comment = FALSE").Scan(&PostDislikesCounter)
+	if err != nil {
+		utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
+		return
+	}
+	err = db.Db.QueryRow("SELECT COUNT(*) FROM likes_dislikes WHERE is_like = TRUE AND is_comment = TRUE").Scan(&CommentsLikeCounter)
+	if err != nil {
+		utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
+		return
+	}
+	err = db.Db.QueryRow("SELECT COUNT(*) FROM likes_dislikes WHERE is_like = FALSE AND is_comment = TRUE").Scan(&CommentsDislikesCounter)
+	if err != nil {
+		utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
+		return
+	}
+	utils.Writer(w, map[string]string{
+		"PostsLikes":       strconv.Itoa(PostLikesCounter),
+		"PostsDislikes":    strconv.Itoa(PostDislikesCounter),
+		"CommentsLikes":    strconv.Itoa(CommentsLikeCounter),
+		"CommentsDislikes": strconv.Itoa(CommentsDislikesCounter),
+	}, 200)
 }
