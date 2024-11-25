6cbbb4da3b5608f36cff9ebebb2f63ac135dea57
zsalhi <rafayelle2005@gmail.com>
2024-11-25 22:47:51 +0100

    update likes api

--- BackEnd/api/Likes.go
+++ BackEnd/api/Likes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 
@@ -25,11 +26,25 @@ func AddLikeAPI(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = db.Db.Exec("INSERT INTO likes_dislikes (post_id, user_id, is_like, is_comment) VALUES (?, ?, ?, ?)", NewLikeOrDislike.PostId, UserID, NewLikeOrDislike.IsLike, NewLikeOrDislike.IsComment)
+	IsLiked, err := AlreadyLiked(UserID, NewLikeOrDislike)
 	if err != nil {
 		utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
 		return
 	}
+	if IsLiked {
+		_, err = db.Db.Exec("DELETE FROM likes_dislikes WHERE post_id = ? AND user_id = ? AND is_like = ? AND is_comment = ?", NewLikeOrDislike.PostId, UserID, NewLikeOrDislike.IsLike, NewLikeOrDislike.IsComment)
+		if err != nil {
+			utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
+			return
+		}
+	} else {
+		_, err = db.Db.Exec("INSERT INTO likes_dislikes (post_id, user_id, is_like, is_comment) VALUES (?, ?, ?, ?)", NewLikeOrDislike.PostId, UserID, NewLikeOrDislike.IsLike, NewLikeOrDislike.IsComment)
+		if err != nil {
+			utils.Writer(w, map[string]string{"Error": err.Error()}, 500)
+			return
+		}
+	}
+
 	var PostLikesCounter, PostDislikesCounter, CommentsLikeCounter, CommentsDislikesCounter int
 
 	err = db.Db.QueryRow("SELECT COUNT(*) FROM likes_dislikes WHERE is_like = TRUE AND is_comment = FALSE").Scan(&PostLikesCounter)
@@ -60,6 +75,16 @@ func AddLikeAPI(w http.ResponseWriter, r *http.Request) {
 	}, 200)
 }
 
-// func AlreadyLiked(userid int, postid int, Islike, Iscomment bool) (bool, error) {
+func AlreadyLiked(userid int, LikesAndDislikes utils.LikesDislikes) (bool, error) {
+	var exists int
+	row := db.Db.QueryRow("SELECT * FROM likes_dislikes WHERE post_id = ? AND user_id = ? AND is_like = ? AND is_comment = ?", LikesAndDislikes.PostId, userid, LikesAndDislikes.IsLike, LikesAndDislikes.IsComment)
+	err := row.Scan(&exists)
+	if err != sql.ErrNoRows {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
 
-// }
+	return true, nil
+}
--- BackEnd/api/allposts.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "net/http"
-
-func AllPosts(w http.ResponseWriter, r *http.Request) {
-	
-}
