80ddc0098387bc91dbd281181585fba1ff845331
zsalhi <rafayelle2005@gmail.com>
2024-11-26 06:37:11 +0100

    starting in frontend

--- BackEnd/Handlers/Main_Handler.go
+++ BackEnd/Handlers/Main_Handler.go
@@ -5,17 +5,18 @@ import (
 	"log"
 	"net/http"
 
-	"forum/BackEnd/helpers"
+	utils "forum/BackEnd/helpers"
 )
 
 func HandleMain(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		helpers.ErrorWriter(w, "Error 404", 404)
+		utils.ErrorWriter(w, "Error 404", 404)
 		return
 	}
 	tem, err := template.ParseFiles("FrontEnd/templates/index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	tem.Execute(w, nil)
 }
---
+++ FrontEnd/static/js/api/Allposts.js
@@ -0,0 +1,16 @@
+// fetch("/api/getposts")
+// .then((res) => res.json())
+// .then((res) => {
+//     LoadData(res.body)
+// }
+// )
+// .catch(err => console.error(err))
+
+// function LoadData(posts) {
+//     const JsonData = JSON.parse(posts)
+//     for (const post in JsonData) {
+//         console.log(post)
+//     }
+
+// }
+
