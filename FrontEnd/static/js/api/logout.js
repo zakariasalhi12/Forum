64fab89ce1e62d1c5ee2a443eb42cca5914d8e4c
zsalhi <rafayelle2005@gmail.com>
2024-11-27 09:18:18 +0100

    fix logout button

--- FrontEnd/static/js/api/logout.js
+++ FrontEnd/static/js/api/logout.js
@@ -1,23 +1,18 @@
-
-
-
-async function DeleteHeader() {
-    const Res = await fetch("api/isloged")
-    const Data = await Res.json()
+async function Logout() {
+    const Res = await fetch("api/isloged");
+    const Data = await Res.json();
     if (Res.status == 200) {
-        const nav = document.getElementById("nav")
-        nav.innerHTML = `<p>Welcome ${Data.username} |</p> <a id="logout">Logout</a>`
+        const nav = document.getElementById("nav");
+        nav.innerHTML = `<p>Welcome ${Data.username} |</p> <a id="logout">Logout</a>`;
+
+        const LogoutButton = document.getElementById("logout");
+        if (LogoutButton) {
+            LogoutButton.addEventListener("click", async (e) => {
+                await fetch("api/logout", { method: "GET" });
+                window.location.href = "/";
+            });
+        }
     }
 }
 
-DeleteHeader()
-
-const LogoutButton = document.getElementById("logout")
-if (LogoutButton) {
-    LogoutButton.addEventListener("click", (e) => {
-        fetch("api/logout", {
-            method: "DELETE",
-        })
-        window.location.href = "/"
-    })
-}
+Logout();
