d9060cb44b95ae2323bf8f06a4febd1d7c453277
zsalhi <rafayelle2005@gmail.com>
2024-11-28 13:39:28 +0100

    add create post in frontend

--- FrontEnd/static/js/api/createpost.js
+++ FrontEnd/static/js/api/createpost.js
@@ -1,11 +1,35 @@
+document.addEventListener("elementReady", () => {
+    const PostButton = document.getElementById("createpost")
+    if (PostButton) {
+        PostButton.addEventListener("click", async () => {
+            console.log("test");
+            const PostTitle = document.getElementById("Posttitle").value
+            const PostContent = document.getElementById("Postcontent").value
+            const PostTopics = document.getElementById("Postopic").value.split(" ")
 
-// fetch("api/post", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({
-//         Title: "tttt",
-//         Content: "tsdqf",
-//         Categories: ["qsdf" , "sdfqsdf"],
-//     }
-//     )
-// })
+            const Res = await fetch("api/post", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    Title: PostTitle,
+                    Content: PostContent,
+                    Categories: PostTopics,
+                }
+                )
+            })
+
+            if (Res.status != 200) {
+                const Data = await Res.json()
+                alert(Data.Error)
+                return
+            }
+
+            alert("Post Created successfuly")
+            window.location.href = "/"
+        })
+    }
+})
+
+
+
+
--- FrontEnd/static/js/api/logout.js
+++ FrontEnd/static/js/api/logout.js
@@ -3,6 +3,36 @@ async function Logout() {
     const Data = await Res.json();
     if (Res.status == 200) {
         const nav = document.getElementById("nav");
+        const cp = document.getElementById("cp")
+        if (cp) {
+            const CreatePost = `
+            <div class="header">
+                <p>Create New Post</p>
+            </div>
+            <div class="fieldsets">
+                <div>
+                    <b>Post Title :</b>
+                    <input id="Posttitle" type="text">
+                    </fieldset>
+                    <fieldset>
+                        <legend>Post Content :</legend>
+                        <textarea id="Postcontent" style="resize: none; width: 100%;" rows="5" id="registeremail" ></textarea>
+                    </fieldset>
+                    <fieldset>
+                        <p>Every topic separet by space</p>
+                        <legend>Post Topics :</legend>
+                        <input id="Postopic" type="text">
+                    </fieldset>
+                    <button id="createpost">Create Post</button>
+                </div>
+            </div>`
+
+            cp.innerHTML = CreatePost
+
+            const elementReadyEvent = new Event("elementReady");
+            document.dispatchEvent(elementReadyEvent);
+
+        }
         if (nav) {
             nav.innerHTML = `<p>Welcome ${Data.username} |</p> <a id="logout">Logout</a>`;
         }
