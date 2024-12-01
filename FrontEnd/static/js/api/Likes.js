c639b63454eda6a5e169c57efdc407744372cdbd
zsalhi <test@test.com>
2024-12-01 02:11:58 +0100

    make like dislike better

--- FrontEnd/static/js/api/Likes.js
+++ FrontEnd/static/js/api/Likes.js
@@ -29,38 +29,31 @@ async function Likeer(Islike, isComment, LikeButton, DislikeButton) {
         )
     })
 
+    if (Res.status != 200) {
+        window.location.href = "/register"
+        return
+    }
+
     const Data = await Res.json()
 
     if (Data.AlreadyLiked) {
-        if (Islike) {
-            LikeButton.style.color = "#222"
-            DislikeButton.style.color = "#222"
-            LikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#222"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/></svg><span>${Data.PostsLikes}</span>`
-            DislikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#222"><path d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"/></svg>${Data.PostsDislikes}</span>`
-            return
-        }
-
-        LikeButton.style.color = "#222"
-        DislikeButton.style.color = "#222"
-        LikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#222"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/></svg><span>${Data.PostsLikes}</span>`
-        DislikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#222"><path d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"/></svg><span>${Data.PostsDislikes}</span>`
+        Like_DisLike_Dom_Handler("#222" , LikeButton , Data.PostsLikes)
+        Like_DisLike_Dom_Handler("#222" , DislikeButton , Data.PostsDislikes)
         return
     }
-
     if (Islike) {
-        LikeButton.style.color = ActiveColor
-        DislikeButton.style.color = "#222"
-        LikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="${ActiveColor}"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/></svg><span>${Data.PostsLikes}</span>`
-        DislikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#222"><path d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"/></svg>${Data.PostsDislikes}</span>`
+        Like_DisLike_Dom_Handler(ActiveColor , LikeButton , Data.PostsLikes)
+        Like_DisLike_Dom_Handler("#222" , DislikeButton , Data.PostsDislikes)
         return
     }
+    Like_DisLike_Dom_Handler("#222" , LikeButton , Data.PostsLikes)
+    Like_DisLike_Dom_Handler(ActiveColor , DislikeButton , Data.PostsDislikes)
+}
 
-    DislikeButton.style.color = ActiveColor
-    LikeButton.style.color = "#222"
-
-    LikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#222"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/></svg><span>${Data.PostsLikes}</span>`
-    DislikeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="${ActiveColor}"><path d="M240-840h440v520L400-40l-50-50q-7-7-11.5-19t-4.5-23v-14l44-174H120q-32 0-56-24t-24-56v-80q0-7 2-15t4-15l120-282q9-20 30-34t44-14Zm360 80H240L120-480v80h360l-54 220 174-174v-406Zm0 406v-406 406Zm80 34v-80h120v-360H680v-80h200v520H680Z"/></svg><span>${Data.PostsDislikes}</span>`
-
+function Like_DisLike_Dom_Handler(color , button , data) {
+    button.style.color = color
+    button.querySelector("svg").setAttribute("fill" , color)
+    button.querySelector("span").innerHTML = data
 }
 
 Like_DisLike()
