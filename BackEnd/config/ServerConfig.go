89a90ea251b8fde870f82a14097347a8018e31a0
zsalhi <test@test.com>
2024-12-09 14:29:33 +0100

    add logs

--- BackEnd/config/ServerConfig.go
+++ BackEnd/config/ServerConfig.go
@@ -1,6 +1,10 @@
 package config
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+	"os"
+)
 
 type ServerConfig struct {
 	Port       string
@@ -17,4 +21,38 @@ var Config = &ServerConfig{
 	ApiLogs:    true,
 	ServerLogs: true,
 	SaveLogs:   true,
+}
+
+func (s *ServerConfig) ApiLogGenerator(str string) {
+	if !s.ApiLogs {
+		return
+	}
+	log.Println(str)
+	if s.SaveLogs {
+		file, err := os.OpenFile("../../Logs/apilogs.txt", os.O_CREATE|os.O_WRONLY, 0o644)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer file.Close()
+		log.SetOutput(file)
+		log.Println(str)
+	}
+}
+
+func (s *ServerConfig) ServerLogGenerator(str string) {
+	if !s.ServerLogs {
+		return
+	}
+	log.Println(str)
+	if s.SaveLogs {
+		file, err := os.OpenFile("../../Logs/serverlogs.txt", os.O_CREATE|os.O_WRONLY, 0o644)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer file.Close()
+		log.SetOutput(file)
+		log.Println(str)
+	}
 }
